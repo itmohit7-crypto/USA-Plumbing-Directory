a08d02ff729a97abd946cab5cc353e6f7f6868c9
itmohit7-crypto <itmohit7@gmail.com>
2026-02-01 12:40:40 +0530

    Create redirect.js

---
+++ redirect.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+    setTimeout(function() {
+        window.location.href = "https://propipeplumbers.thevisitpress.com/";
+    }, 1500); // 1.5 second mein redirect
+};
